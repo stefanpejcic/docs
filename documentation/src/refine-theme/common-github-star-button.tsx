131bf9fbb9ef6d40e4b3b3609c213f830783cc13
Stefan Pejcic <stefan@pejcic.rs>
2024-02-01 17:00:51 +0100

    sasaas

--- documentation/src/refine-theme/common-github-star-button.tsx
+++ documentation/src/refine-theme/common-github-star-button.tsx
@@ -1,8 +1,5 @@
 import clsx from "clsx";
 import React from "react";
-import { useCommunityStatsContext } from "../context/CommunityStats";
-import { GithubIcon } from "./icons/github";
-import { Spinner } from "./spinner";
 
 type Props = {
     className?: string;
@@ -26,7 +23,7 @@ export const CommonGithubStarButton = ({ className }: Props) => {
                 className,
             )}
         >
-            <GithubIcon className={clsx("w-5 h-5")} />
+	<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-message-2-heart" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 9h8" /><path d="M8 13h3.5" /><path d="M10.5 19.5l-1.5 -1.5h-3a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4" /><path d="M18 22l3.35 -3.284a2.143 2.143 0 0 0 .005 -3.071a2.242 2.242 0 0 0 -3.129 -.006l-.224 .22l-.223 -.22a2.242 2.242 0 0 0 -3.128 -.006a2.143 2.143 0 0 0 -.006 3.071l3.355 3.296z" /></svg>
             <div className={clsx("flex items-center", "min-w-[76px] h-6")}>
                     <span
                         className={clsx(
