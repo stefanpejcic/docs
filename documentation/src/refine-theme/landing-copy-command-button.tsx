b7fc829ddd1eb420f0b6069b596e38429b31b123
Stefan Pejcic <stefan@pejcic.rs>
2024-02-04 20:11:40 +0100

    added docker svg

--- documentation/src/refine-theme/landing-copy-command-button.tsx
+++ documentation/src/refine-theme/landing-copy-command-button.tsx
@@ -7,7 +7,7 @@ import {
 } from "./changing-text-element";
 import { LandingCopySuccessIcon } from "./icons/landing-copy-success";
 
-const installText = "wget -qO https://get.openpanel.co";
+const installText = "wget -qO install https://get.openpanel.co && chmod +x install && ./install";
 const copiedText = "copied to clipboard!";
 
 export const LandingCopyCommandButton = ({
